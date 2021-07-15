604f531f98c8f83ebb943b0febbcbdb8a155faf5
prajal patidar <69393777+prajalpatidar06@users.noreply.github.com>
2021-07-15 10:25:24 +0530

    Update map.hpp

--- Map/map.hpp
+++ Map/map.hpp
@@ -30,6 +30,9 @@ protected:
     void preorderTraverse(nod* p);
     void delete_tree(nod* p);
     nod* get_address(T1 key , nod* p);
+    nod* remove(T1 key,nod* p);
+    nod* inorderprefix(nod* p);
+    nod* inordersuffix(nod* p);
 public:
     vector<pair<T1,T2>>arr;
     auto operator()(T1 key , T2 data){
@@ -40,7 +43,7 @@ public:
     int size(){makeUpdate(); return arr.size();}
     bool empty(){makeUpdate(); return (arr.empty())?true:false;}
     void clear(){delete_tree(root); root = NULL;arr.clear();isUpdated = true;}
-    void erase(T1 key);
+    void erase(T1 key){root = remove(key , root);isUpdated = false;}
     nod* find(T1 key);
 };
 
@@ -197,9 +200,101 @@ nod* maptt::find(T1 key){
     return NULL;
 }
 
-// tt
-// void maptt::erase(T1 key){
-//     nod* p = get_address(key,root);
+tt
+nod* maptt::remove(T1 key , nod* p){
+    if(p->key == key){
+        nod*t = NULL,*t2=NULL;
+        int hl,hr;
+        hl = height(p->left);
+        hr = height(p->right);
 
-// }
+        if(hl>hr){
+            t=inorderprefix(p);
+            if(!t && p->left){
+                t2=p->left;
+                p->key=t2->key;
+                p->data = t2->data;
+                p->left = t2->right;
+                delete t2;
+            }else if(!t){
+                t2 = p->right;
+                delete p;
+                return t2;
+            }
+            else{
+                t2 = t->right;
+                p->key = t2->key;
+                p->data = t2->data;
+                t->right = t2->left;
+                delete t2;
+            }
+        }else{
+            t=inordersuffix(p);
+            if(!t && p->right){
+                t2 = p->right;
+                p->key = t2->key;
+                p->data = t2->data;
+                p->right = t2->left;
+                delete t2;
+            }else if(!t){
+                t2 = p->left;
+                delete p;
+                return t2;
+            }
+            else{
+                t2=t->left;
+                p->key = t2->key;
+                p->data = t2->data;
+                t->left = t2->right;
+                delete t2;
+            }
+        }
+    }
+    else if(p->key > key)
+        p->left = remove(key , p->left);
+    else if(p->key < key)
+        p->right = remove(key , p->right);
+    p->height = height(p);
+   int bf = balance_factor(p);
+
+    if(bf == 0 || bf == -1 || bf == 1)return p;
+    else if(bf == -2){
+        if(balance_factor(p->right) == 1)
+            p = RLrotation(p);
+        else
+            p = RRrotation(p);
+    }else if(bf == 2){
+        if(balance_factor(p->left) == 1)
+            p = LLrotation(p);
+        else
+            p = LRrotation(p);
+    }
+    return p;
+}
+
+tt
+nod* maptt::inorderprefix(nod*p){
+    nod*t=NULL;
+    if(p->left){
+        p = p->left;
+        while(p->right){
+            t = p;
+            p = p->right;
+        }
+    }
+    return t;
+}
+
+tt
+nod* maptt::inordersuffix(nod*p){
+    nod*t=NULL;
+    if(p->right){
+        p = p->right;
+        while(p->left){
+            t = p;
+            p = p->left;
+        }
+    }
+    return t;
+}
 
